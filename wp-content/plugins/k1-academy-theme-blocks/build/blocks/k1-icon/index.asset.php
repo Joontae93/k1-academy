22d5adac68aa763f3eda548e1883e69303e36d91
kjroelke <kj.roelke@gmail.com>
2024-02-26 12:22:02 -0600

    Finish up k1-icon block

--- wp-content/plugins/k1-academy-theme-blocks/build/blocks/k1-icon/index.asset.php
+++ wp-content/plugins/k1-academy-theme-blocks/build/blocks/k1-icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '740e11fdec86042a79ff');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '4ae8505965aaac44185f');
